b167f424e4544b667f15d820711937ae53985711
Mohammedwich <45858931+Mohammedwich@users.noreply.github.com>
2019-10-13 23:54:26 -0500

    Added try-catch for errors, freed allocated memory

--- Inheritance Program 4 HW/src/main.cpp
+++ Inheritance Program 4 HW/src/main.cpp
@@ -45,248 +45,261 @@ int main()
 		return -1;
 	}
 
-
-	while(getline(fileReader, line))
+//try start
+	try
 	{
-		//Get rid of commas from string
-		for(auto iter = line.begin(); iter!=line.end(); ++iter)
-		   {
-		       if((*iter)==',')
-		       {
-		           line.erase(iter);
-		       }
-		   }
-
-		std::string command;
-		int typeToBuildOrPrint;
-		std::string word;
-		std::stringstream s(line);
-
-		s >> command;
-		s >> word;
-		typeToBuildOrPrint = stoi(word);
-
-		//check if we need to skip line
-		if (!(command.compare("n") || command.compare("p")) )
+		while(getline(fileReader, line))
 		{
-			cout << "Invalid command or object type. Skipping line..." << endl;
-			continue;
-		}
+			//Get rid of commas from string
+			for(auto iter = line.begin(); iter!=line.end(); ++iter)
+			   {
+				   if((*iter)==',')
+				   {
+					   line.erase(iter);
+				   }
+			   }
 
+			std::string command;
+			int typeToBuildOrPrint;
+			std::string word;
+			std::stringstream s(line);
 
-		if(command.compare("n"))
-		{
-			if(typeToBuildOrPrint == 0)
+			s >> command;
+			s >> word;
+			typeToBuildOrPrint = stoi(word);
+
+			//check if we need to skip line
+			if (!(command.compare("n") || command.compare("p")) )
 			{
-				int seats;
-				int engines;
-
-				s >> word;
-				seats = stoi(word);
-				s >> word;
-				engines = stoi(word);
-
-				planesVector.push_back(new Aircraft(seats, engines));
+				cout << "Invalid command or object type. Skipping line..." << endl;
+				continue;
 			}
-			else if(typeToBuildOrPrint == 1)
+
+
+			if(command.compare("n"))
 			{
-				int seats;
-				int engines;
-
-
-				s >> word;
-				seats = stoi(word);
-				s >> word;
-				engines = stoi(word);
-
-				std::string::iterator firstWordStart = nullptr;
-				std::string::iterator firstWordEnd = nullptr;
-
-				//Getting the range of the phrase from the line to initialize a string with said range, so it can be passed to constructor
-				for(auto iter = line.begin(); iter != line.end(); ++iter)
+				if(typeToBuildOrPrint == 0)
 				{
-					if((*iter) == '"')
+					int seats;
+					int engines;
+
+					s >> word;
+					seats = stoi(word);
+					s >> word;
+					engines = stoi(word);
+
+					planesVector.push_back(new Aircraft(seats, engines));
+				}
+				else if(typeToBuildOrPrint == 1)
+				{
+					int seats;
+					int engines;
+
+
+					s >> word;
+					seats = stoi(word);
+					s >> word;
+					engines = stoi(word);
+
+					std::string::iterator firstWordStart = nullptr;
+					std::string::iterator firstWordEnd = nullptr;
+
+					//Getting the range of the phrase from the line to initialize a string with said range, so it can be passed to constructor
+					for(auto iter = line.begin(); iter != line.end(); ++iter)
 					{
-						if(firstWordStart == nullptr)
+						if((*iter) == '"')
 						{
-							firstWordStart = (iter+1);
-						}
-						else if(firstWordEnd == nullptr)
-						{
-							firstWordEnd = iter;
-						}
-						else
-						{
-							cout << "Issue at string range detector" << endl;
-							return -3;
+							if(firstWordStart == nullptr)
+							{
+								firstWordStart = (iter+1);
+							}
+							else if(firstWordEnd == nullptr)
+							{
+								firstWordEnd = iter;
+							}
+							else
+							{
+								cout << "Issue at string range detector" << endl;
+								return -3;
+							}
 						}
 					}
+
+					std::string description(firstWordStart, firstWordEnd);
+
+					planesVector.push_back(new Fighter(seats, engines, description));
+
 				}
-
-				std::string description(firstWordStart, firstWordEnd);
-
-				planesVector.push_back(new Fighter(seats, engines, description));
-
-			}
-			else if(typeToBuildOrPrint == 2)
-			{
-				int seats;
-				int engines;
-
-				s >> word;
-				seats = stoi(word);
-				s >> word;
-				engines = stoi(word);
-
-				std::string::iterator firstWordStart = nullptr;
-				std::string::iterator firstWordEnd = nullptr;
-				std::string::iterator secondWordStart = nullptr;
-				std::string::iterator secondWordEnd = nullptr;
-
-				//Getting the range of the phrases from the line to initialize a string with said range, so it can be passed to constructor
-				for(auto iter = line.begin(); iter != line.end(); ++iter)
+				else if(typeToBuildOrPrint == 2)
 				{
-					if((*iter) == '"')
+					int seats;
+					int engines;
+
+					s >> word;
+					seats = stoi(word);
+					s >> word;
+					engines = stoi(word);
+
+					std::string::iterator firstWordStart = nullptr;
+					std::string::iterator firstWordEnd = nullptr;
+					std::string::iterator secondWordStart = nullptr;
+					std::string::iterator secondWordEnd = nullptr;
+
+					//Getting the range of the phrases from the line to initialize a string with said range, so it can be passed to constructor
+					for(auto iter = line.begin(); iter != line.end(); ++iter)
 					{
-						if(firstWordStart == nullptr)
+						if((*iter) == '"')
 						{
-							firstWordStart = (iter+1);
-						}
-						else if(firstWordEnd == nullptr)
-						{
-							firstWordEnd = iter;
-						}
-						else if(secondWordStart == nullptr)
-						{
-							secondWordStart = (iter+1);
-						}
-						else if(secondWordEnd == nullptr)
-						{
-							secondWordEnd = iter;
-						}
-						else
-						{
-							cout << "Issue at string range detector" << endl;
-							return -3;
+							if(firstWordStart == nullptr)
+							{
+								firstWordStart = (iter+1);
+							}
+							else if(firstWordEnd == nullptr)
+							{
+								firstWordEnd = iter;
+							}
+							else if(secondWordStart == nullptr)
+							{
+								secondWordStart = (iter+1);
+							}
+							else if(secondWordEnd == nullptr)
+							{
+								secondWordEnd = iter;
+							}
+							else
+							{
+								cout << "Issue at string range detector" << endl;
+								return -3;
+							}
 						}
 					}
+
+					std::string description1(firstWordStart, firstWordEnd);
+					std::string description2(secondWordStart, secondWordEnd);
+
+					planesVector.push_back(new Acrobat(seats, engines, description1, description2));
+
 				}
-
-				std::string description1(firstWordStart, firstWordEnd);
-				std::string description2(secondWordStart, secondWordEnd);
-
-				planesVector.push_back(new Acrobat(seats, engines, description1, description2));
-
-			}
-			else if(typeToBuildOrPrint == 3)
-			{
-
-				int seats;
-				int engines;
-
-				s >> word;
-				seats = stoi(word);
-				s >> word;
-				engines = stoi(word);
-
-				std::string::iterator firstWordStart = nullptr;
-				std::string::iterator firstWordEnd = nullptr;
-				std::string::iterator secondWordStart = nullptr;
-				std::string::iterator secondWordEnd = nullptr;
-
-				//Getting the range of the phrases from the line to initialize a string with said range, so it can be passed to constructor
-				for(auto iter = line.begin(); iter != line.end(); ++iter)
+				else if(typeToBuildOrPrint == 3)
 				{
-					if((*iter) == '"')
+
+					int seats;
+					int engines;
+
+					s >> word;
+					seats = stoi(word);
+					s >> word;
+					engines = stoi(word);
+
+					std::string::iterator firstWordStart = nullptr;
+					std::string::iterator firstWordEnd = nullptr;
+					std::string::iterator secondWordStart = nullptr;
+					std::string::iterator secondWordEnd = nullptr;
+
+					//Getting the range of the phrases from the line to initialize a string with said range, so it can be passed to constructor
+					for(auto iter = line.begin(); iter != line.end(); ++iter)
 					{
-						if(firstWordStart == nullptr)
+						if((*iter) == '"')
 						{
-							firstWordStart = (iter+1);
-						}
-						else if(firstWordEnd == nullptr)
-						{
-							firstWordEnd = iter;
-						}
-						else if(secondWordStart == nullptr)
-						{
-							secondWordStart = (iter+1);
-						}
-						else if(secondWordEnd == nullptr)
-						{
-							secondWordEnd = iter;
-						}
-						else
-						{
-							cout << "Issue at string range detector" << endl;
-							return -3;
+							if(firstWordStart == nullptr)
+							{
+								firstWordStart = (iter+1);
+							}
+							else if(firstWordEnd == nullptr)
+							{
+								firstWordEnd = iter;
+							}
+							else if(secondWordStart == nullptr)
+							{
+								secondWordStart = (iter+1);
+							}
+							else if(secondWordEnd == nullptr)
+							{
+								secondWordEnd = iter;
+							}
+							else
+							{
+								cout << "Issue at string range detector" << endl;
+								return -3;
+							}
 						}
 					}
+
+					std::string description1(firstWordStart, firstWordEnd);
+					std::string description2(secondWordStart, secondWordEnd);
+
+					planesVector.push_back(new Freight(seats, engines, description1, description2));
+
 				}
-
-				std::string description1(firstWordStart, firstWordEnd);
-				std::string description2(secondWordStart, secondWordEnd);
-
-				planesVector.push_back(new Freight(seats, engines, description1, description2));
-
-			}
-			else
-			{
-				cout << "An error occurred while reading the file. Exiting" << endl;
-				return -2;
-			}
-		}
-		else if (command.compare("p"))
-		{
-			if(typeToBuildOrPrint == 0)
-			{
-				for(auto iter = planesVector.begin(); iter != planesVector.end(); ++iter)
+				else
 				{
-					(*iter)->printCharacteristics();
+					cout << "An error occurred while reading the file. Exiting" << endl;
+					return -2;
 				}
 			}
-			else if(typeToBuildOrPrint == 1)
+			else if (command.compare("p"))
 			{
-				for(auto iter = planesVector.begin(); iter != planesVector.end(); ++iter)
+				if(typeToBuildOrPrint == 0)
 				{
-					if((*iter)->ObjectTypeID() == 1)
+					for(auto iter = planesVector.begin(); iter != planesVector.end(); ++iter)
 					{
 						(*iter)->printCharacteristics();
 					}
 				}
-			}
-			else if(typeToBuildOrPrint == 2)
-			{
-				for(auto iter = planesVector.begin(); iter != planesVector.end(); ++iter)
+				else if(typeToBuildOrPrint == 1)
 				{
-					if((*iter)->ObjectTypeID() == 2)
+					for(auto iter = planesVector.begin(); iter != planesVector.end(); ++iter)
 					{
-						(*iter)->printCharacteristics();
+						if((*iter)->ObjectTypeID() == 1)
+						{
+							(*iter)->printCharacteristics();
+						}
 					}
 				}
-			}
-			else if(typeToBuildOrPrint == 3)
-			{
-				for(auto iter = planesVector.begin(); iter != planesVector.end(); ++iter)
+				else if(typeToBuildOrPrint == 2)
 				{
-					if((*iter)->ObjectTypeID() == 3)
+					for(auto iter = planesVector.begin(); iter != planesVector.end(); ++iter)
 					{
-						(*iter)->printCharacteristics();
+						if((*iter)->ObjectTypeID() == 2)
+						{
+							(*iter)->printCharacteristics();
+						}
 					}
 				}
-			}
-			else
-			{
-				cout << "Program broke while trying to execute print command" << endl;
-				return -4;
+				else if(typeToBuildOrPrint == 3)
+				{
+					for(auto iter = planesVector.begin(); iter != planesVector.end(); ++iter)
+					{
+						if((*iter)->ObjectTypeID() == 3)
+						{
+							(*iter)->printCharacteristics();
+						}
+					}
+				}
+				else
+				{
+					cout << "Program broke while trying to execute print command" << endl;
+					return -4;
+				}
+
 			}
 
 		}
-
 	}
+	catch(...)
+	{
+		cout << "Invalid arguments. Skipping to next line of input..." << endl;
+		continue;
+	}
+	//try block end
 
 
-
-// TODO: Free allocated memory
+	//Free allocated memory
+	for(auto x : planesVector)
+	{
+		delete x;
+	}
+	planesVector.clear();
 
 	return 0;
 }
